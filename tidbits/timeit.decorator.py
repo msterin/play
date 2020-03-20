69b44d094727e07f21f0772c2f622a9c464c79b6
Mark Sterin <msterin@kontain.app>
2020-03-20 14:38:26 -0700

    tidbits

---
+++ tidbits/timeit.decorator.py
@@ -0,0 +1,9 @@
+def timeit_wrapper(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()  # Alternatively, you can use time.process_time()
+        func_return_val = func(*args, **kwargs)
+        end = time.perf_counter()
+        print('{0:<10}.{1:<8} : {2:<8}'.format(func.__module__, func.__name__, end - start))
+        return func_return_val
+    return wrapper
